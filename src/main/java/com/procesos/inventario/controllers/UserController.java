5701801375d35924038178a3bf2f4133194dd012
Yindry Ascanio <ymascaniop@ufpso.edu.co>
2023-03-21 20:51:03 -0500

    Se agregaron consultas

--- src/main/java/com/procesos/inventario/controllers/UserController.java
+++ src/main/java/com/procesos/inventario/controllers/UserController.java
@@ -3,23 +3,56 @@ package com.procesos.inventario.controllers;
 import com.procesos.inventario.models.User;
 import com.procesos.inventario.services.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 public class UserController {
 
     @Autowired
-    private UserServiceImp userService;
+    private UserServiceImp userServiceImp;
 
     @GetMapping(value = "/user/{id}")
-    public User findUserById(@PathVariable Long id){
-        return userService.getUser(id);
+    public ResponseEntity findUserById(@PathVariable Long id){
+        Map response = new HashMap();
+        try {
+            return new ResponseEntity(userServiceImp.getUser(id), HttpStatus.OK);
+        }catch(Exception e){
+            response.put("status","404");
+            response.put("message","No se encontre el usuario");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+
+        }
     }
     @PostMapping(value="user")
-    public Boolean saveUser(@RequestBody User user){
-        return UserServiceImp.createUser(user);
+    public ResponseEntity saveUser(@RequestBody User user){
+        Map response = new HashMap();
+        Boolean userResp = userServiceImp.createUser(user);
+        if(userResp == true){
+            response.put("status","201");
+            response.put("message","se creo el usuario");
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        }
+        response.put("status","400");
+        response.put("message","Hubo un error al crear el usuario");
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+    }
+    @GetMapping(value = "/users")
+    public ResponseEntity findUserByAll(){
+        Map response = new HashMap();
+        try {
+            return new ResponseEntity(userServiceImp.allUsers(), HttpStatus.OK);
+        }catch(Exception e){
+            response.put("status","409");
+            response.put("message","Estos son todos los usuarios");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+
+        }
     }
 }
 
